e7db616c29383b7916ca32c83568f13d9c75b0bc
Ralph Greaves <rgreaves@ycp.edu>
2023-03-17 02:11:37 -0400

    Update index.js

--- packages/package/function/index.js
+++ packages/package/function/index.js
@@ -1,14 +1,6 @@
 const mysqlx = require('@mysql/xdevapi');
 
 async function main(args) {
-  try {
-    const session = await mysqlx.getSession('mysql://doadmin:AVNS_1OhWgGQoFCngSBucL6b@db-mysql-nyc1-37474-do-user-12845046-0.b.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED');
-    
-    session.close();
-  } catch (err) {
-    return { err };
-  }
-
   return {
     headers: {
       'Content-Type': 'application/json'
