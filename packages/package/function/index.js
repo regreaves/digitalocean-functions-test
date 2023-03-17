2dda8c749fc655637c3816218615c386766764c2
Ralph Greaves <rgreaves@ycp.edu>
2023-03-17 03:57:56 -0400

    Update index.js

--- packages/package/function/index.js
+++ packages/package/function/index.js
@@ -1,11 +1,21 @@
-const mysqlx = require('@mysql/xdevapi');
-
 async function main(args) {
-  const config = "mysql://doadmin:AVNS_1OhWgGQoFCngSBucL6b@db-mysql-nyc1-37474-do-user-12845046-0.b.db.ondigitalocean.com:25064/defaultdb?ssl-mode=REQUIRED";
+  const mysqlx = require('@mysql/xdevapi');
+
+  const config = {
+    user: process.env.MYSQLX_USER,
+    password: process.env.MYSQLX_PASSWORD,
+    host: process.env.MYSQLX_HOST,
+    port: Number.parseInt(process.env.MYSQLX_PORT),
+    schema: process.env.MYSQLX_SCHEMA,
+    tls: {
+      enabled: true
+    }
+  };
 
   const session = await mysqlx.getSession(config);
 
-  const data = session.inspect();
+  const response = await session.sql('CALL list_hardware_items;').execute();
+  const data = await response.fetchAll();
 
   session.close();
 
