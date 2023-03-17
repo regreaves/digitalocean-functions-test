7b4dffb271e428a492a0cdbf9dd9384de9166edc
Ralph Greaves <rgreaves@ycp.edu>
2023-03-17 11:58:24 -0400

    Update index.js

--- packages/package/function/index.js
+++ packages/package/function/index.js
@@ -1,25 +1,5 @@
 async function main(args) {
-  const mysqlx = require('@mysql/xdevapi');
-
-  const config = {
-    user: process.env.MYSQLX_USER,
-    password: process.env.MYSQLX_PASSWORD,
-    host: process.env.MYSQLX_HOST,
-    port: Number.parseInt(process.env.MYSQLX_PORT),
-    schema: process.env.MYSQLX_SCHEMA,
-    tls: {
-      enabled: true
-    }
-  };
-
-  const session = await mysqlx.getSession(config);
-
-  const response = await session.sql('CALL list_hardware_items;').execute();
-  const data = await response.fetchAll();
-
-  session.close();
-
-  return { body: data };
+  return { body: "content" };
 }
 
 module.exports = { main };
