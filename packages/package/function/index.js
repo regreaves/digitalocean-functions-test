2ceaa70dc3a93bc1bdd31c981ca74f513ddfebd5
Ralph Greaves <rgreaves@ycp.edu>
2023-03-17 03:07:33 -0400

    Update index.js

--- packages/package/function/index.js
+++ packages/package/function/index.js
@@ -1,19 +1,26 @@
+require('dotenv').config();
+
 const mysqlx = require('@mysql/xdevapi');
 
 async function main(args) {
-  const session = await mysqlx.getSession($DATABASE_URL);
+  const config = {
+    user: process.env.MYSQLX_USER,
+    password: process.env.MYSQLX_PASSWORD,
+    host: process.env.MYSQLX_HOST,
+    port: Number.parseInt(process.env.MYSQLX_PORT),
+    schema: process.env.MYSQLX_SCHEMA,
+    tls: {
+      enabled: true
+    }
+  };
+
+  const session = await mysqlx.getSession(config);
 
   const data = session.inspect();
 
   session.close();
 
-  return {
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    statusCode: 200,
-    body: `Hello, ${args?.name || "stranger"}! data: ${data}`
-  }
+  return { body: data };
 }
 
 module.exports = { main };
